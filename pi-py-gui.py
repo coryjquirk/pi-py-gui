dcc41229077ff293b5ac89b4b07899d7d2658616
coryjquirk <coryjquirk@gmail.com>
2021-03-18 23:17:09 -0500

    added waffle module

--- pi-py-gui.py
+++ pi-py-gui.py
@@ -1,19 +1,19 @@
-from guizero import App, Text, TextBox, PushButton, Slider, Picture
+from guizero import App, Text, TextBox, PushButton, Slider, Picture, Waffle
 
 app = App(title="truth machine")
 
-welcome_message = Text(app, text="yo check this out", size=34, font="Helvetica", color="#9932CC")
+welcome_message = Text(app, text="you pick", size=34, font="Helvetica", color="#96b0a3")
 my_name = TextBox(app, width=25)
 
 def say_my_name():
     welcome_message.value = my_name.value
-update_text = PushButton(app, command=say_my_name, text="say it to the world")
+update_text = PushButton(app, command=say_my_name, text="say", pady=10)
 
 def change_text_size(slider_value):
     welcome_message.size = slider_value
 
 text_size = Slider(app, command=change_text_size, start=10, end=80)
 
-my_cat = Picture(app, image="green.gif")
+waffle = Waffle(app, align="bottom", color="#6b8f7d", height=25, width=25, dotty="true")
 
 app.display()
